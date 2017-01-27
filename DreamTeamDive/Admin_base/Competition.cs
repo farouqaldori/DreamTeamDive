03ef63292e9fe00b3528b0f0b9068261a3ffc47a
Gabbee7 <gabbelito96@gmail.com>
2017-01-27 13:11:26 +0100

    Creating classes for Competition and Databse in Admin_form

---
+++ DreamTeamDive/Admin_base/Competition.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Admin_base
+{
+    class Competition
+    {
+        public int id { get; set; }
+        public int type { get; set; }
+        public int date { get; set; }
+        public int[] number { get; set; }
+        
+       public Competition()
+       {
+            id = -1;
+            type = 0;
+            date = 0;
+       }
+
+        public void generate()
+        {
+
+        }
+
+        public void addDiver()
+        {
+
+        }
+
+        public void addJudge()
+        {
+
+        }
+
+        public void start()
+        {
+
+        }
+    }
+
+    
+
+    
+}
