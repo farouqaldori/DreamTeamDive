59504d09fc18208721276bc3be5f81ef3ce745a7
theycallmesmile <killen163@hotmail.com>
2017-01-27 13:12:20 +0100

    Creating classes for diver and judge in member form.

---
+++ DreamTeamDive/Base/Diver.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Base
+{
+    class Diver
+    {
+
+        public int id { get; set; }
+        int authCode { get; set; }
+        string name { get; set; }
+        string country { get; set; }
+        int competition { get; set; }
+
+
+        public Diver()
+        {
+            int id;
+            int authCode;
+            string name;
+            string country;
+            string competition;
+        }
+
+        public void Auth()
+        {
+
+        }
+
+        public void jump()
+        {
+
+        }
+    }
+}
---
+++ DreamTeamDive/Base/Judge.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Base
+{
+    class Judge
+    {
+
+        public int id { get; set; }
+        int authCode { get; set; }
+        string name { get; set; }
+        int competition { get; set; }
+
+        public Judge()
+        {
+            int id;
+            int authCode;
+            string name;
+            string competition;
+        }
+
+        public void Auth()
+        {
+
+        }
+
+        public void Rate(int jumpId)
+        {
+
+        }
+    }
+}
