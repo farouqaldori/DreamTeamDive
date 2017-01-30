e53be07fdf8e3bf28071f067e494d1fa171989a4
Ako <saman.nisstany@hotmail.com>
2017-01-30 11:45:24 +0100

    Added opttion to save and open files (.shp)

--- DreamTeamDive/Admin_base/main_form.cs
+++ DreamTeamDive/Admin_base/main_form.cs
@@ -39,24 +39,50 @@ namespace Admin_base
         
         private void öppnaToolStripMenuItem_Click(object sender, EventArgs e)
         {
-            //Predefined extention to standard "open Window"
-            OpenFileDialog open = new OpenFileDialog();
-            if(open.ShowDialog()==DialogResult.OK)
+            Stream myStream = null;
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+
+            openFileDialog1.InitialDirectory="c:\\";
+            openFileDialog1.Filter="shp files (*.shp)|*.shp|All files (*.*)|*.*";
+            openFileDialog1.FilterIndex=2;
+            openFileDialog1.RestoreDirectory=true;
+
+            if(openFileDialog1.ShowDialog()==DialogResult.OK)
             {
-                StreamReader reader = new StreamReader(File.OpenRead(open.FileName));
-                //To do: ways to open a specific file
+                try
+                {
+                    if((myStream=openFileDialog1.OpenFile())!=null)
+                    {
+                        using(myStream)
+                        {
+                            // Insert code to read the stream here.
+                        }
+                    }
+                } catch(Exception ex)
+                {
+                    MessageBox.Show("Error: Could not read file from disk. Original error: "+ex.Message);
+                }
             }
         }
 
         private void sparaSomToolStripMenuItem_Click(object sender, EventArgs e)
         {
-            //Predefined extention to standard "open Window"
-            OpenFileDialog open = new OpenFileDialog();
-            if(open.ShowDialog()==DialogResult.OK)
+            //Predefined extention to standard "save Window"
+            Stream myStream;
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "shp file (*.shp)|*.shp|All files (*.*)|*.*";
+            saveFileDialog.FilterIndex = 2;
+            saveFileDialog.RestoreDirectory = true;
+
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
             {
-                StreamReader reader = new StreamReader(File.OpenRead(open.FileName));
-                //To-do: ways to save a specific file
+                if ((myStream =saveFileDialog.OpenFile()) != null)
+                {
+                    // code to write the stream goes here
+                    myStream.Close();
+                }
             }
+
         }
     }
 }
